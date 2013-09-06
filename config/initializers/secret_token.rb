c28d48e21536c5184e04f05814f8cd7cb2902aad
Shalmali <shalmali.rajadhyax@west.cmu.edu>
2013-09-06 13:16:53 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Listomania::Application.config.secret_token = 'f53e6736179de000be0b6d8bfe0636a16885bda788c952e8f6bf5ccfe159bfc7b1a19e54ab73fa822e989ba89067f35d948b817cad7771ec218571cbd5dc6afc'
