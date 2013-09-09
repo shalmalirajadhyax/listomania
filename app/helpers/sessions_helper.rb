f84d51e5ffa4faf12c0009d217e88a03cb40dfa2
Shalmali <shalmali.rajadhyax@west.cmu.edu>
2013-09-08 23:15:21 -0700

    Signout changes

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -9,7 +9,9 @@ module SessionsHelper
 	end
 	
 	def current_user
+		if cookies[:remember_token]
 		@current_user ||= User.find_by_remember_token(cookies[:remember_token])
+		end
 	end
 	
 	def signed_in?
