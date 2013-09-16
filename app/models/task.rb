215fd576e7458957c86c2db19257e07d5c9b5399
shalmalirajadhyax <shalmali.rajadhyax@west.cmu.edu>
2013-09-16 10:18:57 -0700

    Update task.rb

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: tasks
+#
+#  id            :integer          not null, primary key
+#  task_name     :string(255)
+#  task_category :string(255)
+#  user_id       :integer
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#
+
 class Task < ActiveRecord::Base
   attr_accessible :task_category, :task_name
   belongs_to :user
@@ -7,4 +19,11 @@ class Task < ActiveRecord::Base
   validates :task_category, presence: true
   validates_inclusion_of :task_category, in: %w(Home School Shopping)
   
+  def self.search(search)
+	if search
+		where('task_name LIKE?', "%#{search}%")
+	else
+		scoped
+	end
+end
 end
